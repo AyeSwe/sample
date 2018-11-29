31bb188c73faf580c320a75aa194aebf4eb08ac3
Aye Swe <ayeswe@MacBook-Pro.local>
2018-11-28 19:34:09 -0800

    First commit

---
+++ src/dynameWeb/Main.java
@@ -0,0 +1,23 @@
+package dynameWeb;
+
+public class Main {
+	
+	
+	public static void main(String [] args) {
+		
+		int result = doMath(2,3);
+		
+			System.out.println("result is : " + result);
+			
+			
+		}
+		
+		public static int doMath(int a, int b) {
+			
+			int result = a + b; 
+		return result;	
+		}
+		
+	}
+
+
---
+++ src/dynameWeb/Test.java
@@ -0,0 +1,17 @@
+package dynameWeb;
+
+import static org.junit.Assert.assertEquals;
+
+public class Test {
+
+	@org.junit.Test
+        public void BearerKey_test() {
+               
+		int result = Main.doMath(3, 2);
+		assertEquals(5, result);
+		
+		
+        }
+
+}
+
